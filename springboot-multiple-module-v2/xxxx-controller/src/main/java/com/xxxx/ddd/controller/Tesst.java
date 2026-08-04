8a2f4f0931b6b6498f92ecfd84e3027b95abe791
boyquynhluu <vanductai.dhv@gmail.com>
2026-08-05 00:12:14 +0700

    Clean repository and ignore build artifacts

--- springboot-multiple-module-v2/xxxx-controller/src/main/java/com/xxxx/ddd/controller/Tesst.java
+++ springboot-multiple-module-v2/xxxx-controller/src/main/java/com/xxxx/ddd/controller/Tesst.java
@@ -3,6 +3,6 @@ package com.xxxx.ddd.controller;
 public class Tesst {
 
     public static void main(String[] args) {
-        System.out.println("Test Github Actions CI");
+        System.out.println("Test Github Actions CI @v4");
     }
 }
